8db83df52017923ca3a265dd87ab9263286be653
cmolea <cmolea@unitedmotionideas.com>
2015-10-12 15:55:45 +0200

    Clean the movie title.

--- index.js
+++ index.js
@@ -14,30 +14,38 @@ var index = 0;
 
 _.forEach(movies, function (movie) {
     seriesArray.push(function (seriesCallback) {
-        var params = querystring.stringify({
-            title: movie.content,
-            lang: 'en'
-        });
-        request.send({
-            host: 'popcorngarage.com',
-            path: '/scripts/lookup.php',
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/x-www-form-urlencoded',
-                'Content-Length': params.length
-            }
-        }, params)
-        .then(function (result) {
-            parseString(result, function (err, data) {
-                if (data.xml.badgeid) {
-                    console.log(++index + '. ' + data.xml.badgeid + ' - ' + data.xml.movie);
-                }
-                seriesCallback(null);
-            });
-        })
-        .fail(function (err) {
-            seriesCallback(err);
-        });
+        var movieTitle = movie.content.toLowerCase().replace(/\W/g, '');
+
+        if (movieTitle.length > 1) {
+            setTimeout(function () {
+                var params = querystring.stringify({
+                    title: movieTitle,
+                    lang: 'en'
+                });
+                request.send({
+                    host: 'www.popcorngarage.com',
+                    path: '/scripts/lookup.php',
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/x-www-form-urlencoded',
+                        'Content-Length': params.length
+                    }
+                }, params)
+                .then(function (result) {
+                    parseString(result, function (err, data) {
+                        if (data && data.xml && data.xml.badgeid) {
+                            console.log(++index + '. ' + data.xml.badgeid + ' - ' + data.xml.movie);
+                        }
+                        seriesCallback(null);
+                    });
+                })
+                .fail(function (err) {
+                    seriesCallback(err);
+                });
+            }, 1000);
+        } else {
+            seriesCallback(null);
+        }
     });
 });
 
